77b5e7796e8a1b7edd6049d280708a40bcaec6d6
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-05 16:56:04 -0700

    set up ThemeContext consumers

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -6,13 +6,24 @@ export const ThemeContext = createContext();
 export class ThemeProvider extends Component {
   constructor(props) {
     super(props);
-    this.state = { isDarkMode: true };
+    this.state = { isDarkMode: false };
+    this.toggleTheme = this.toggleTheme.bind(this);
+  }
+
+  toggleTheme() {
+    this.setState({ isDarkMode: !this.state.isDarkMode });
   }
 
   render() {
     // Provides children with value: One value per context
     return (
-      <ThemeContext.Provider value={{ ...this.state }}>
+      <ThemeContext.Provider
+        value={{
+          ...this.state,
+          tastesLikeChicken: true,
+          toggleTheme: this.toggleTheme,
+        }}
+      >
         {this.props.children}
       </ThemeContext.Provider>
     );
