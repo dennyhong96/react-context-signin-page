d016385974533bf1308f4aded65628c215413bb7
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-05 15:56:52 -0700

    add Form component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
-import "./App.css";
+import Navbar from "./Navbar";
+import Form from "./Form";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Navbar />;
+      <Form />
+    </>
+  );
 }
 
 export default App;
